f7af01504b3f7a6b8f22ddae791c3449067d94b6
Chrissy Cooper <cooper.christine.r@gmail.com>
2023-04-08 18:27:15 -0500

    feat: Build out basic server

--- server/index.js
+++ server/index.js
@@ -1,3 +1,13 @@
-const express = require("express");
-const app = express();
+//run nodemon index or node index to start server
 
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+//middleware
+app.use(cors());
+app.use(express.json()); //allowing us to have access to request.body so we can get json data
+
+app.listen(5000, () => {
+  console.log('server has started on port 5000')
+})
