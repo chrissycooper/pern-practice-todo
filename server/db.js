81d114f4a2b936ea7f27de8cdb1ed67f92e83ce7
Chrissy Cooper <cooper.christine.r@gmail.com>
2023-04-15 13:44:13 -0500

    feat: Add serverside GET, POST, PUT, and DELETE requests

--- server/db.js
+++ server/db.js
@@ -1,7 +1,7 @@
 const Pool = require("pg").Pool
 
 const pool = new Pool({
-  user: 'postgres',
+  user: 'chrissy',
   password: 'jimminychristmas',
   host: 'localhost',
   port: 5432,
--- server/index.js
+++ server/index.js
@@ -13,13 +13,65 @@ app.use(express.json()); //allowing us to have access to request.body so we can 
 
 //create a todo
 
+app.post('/todos', async(req, res) => {
+  try {
+    const { description } = req.body;
+    const newTodo = await pool.query("INSERT INTO todo (description) VALUES($1) RETURNING *", [description]
+    );
+
+    res.json(newTodo.rows[0]);
+
+  } catch (err) {
+    console.error(err.message)
+  }
+})
+
 //get all todos
+app.get('/todos', async(req, res) => {
+  try {
+    const allTodos = await pool.query("SELECT * FROM todo")
+    res.json(allTodos.rows)
+  } catch (err) {
+    console.error(err.message)
+  }
+})
 
 //get a todo
-
+app.get('/todos/:id', async(req, res) => {
+  try {
+    const { id } = req.params
+    const singleTodo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [id])
+    res.json(singleTodo.rows[0])
+  } catch (err) {
+    console.error(err.message)
+    
+  }
+})
+//$1 and $2 are variable holders and they directly relate to the array that's the second argument of the query
+//in plain language that query is saying 'update the todo table, set that table with the description which will be the first item in the array, and do that in the place setting (table row) where the id is equal to the second item in the following array (name tag matches)
 //update a todo
+app.put('/todos/:id', async(req, res) => {
+  try {
+    const {id} = req.params
+    const {description} = req.body
+    const updateTodo = await pool
+    .query("UPDATE todo SET description = $1 WHERE todo_id = $2", [description, id])
+    res.json("response was updated!")
+  } catch (err) {
+    console.error(err.message)
+  }
+})
 
 //delete a todo
+app.delete('/todos/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteTodo = await pool.query("DELETE FROM todo WHERE todo_id = $1", [id])
+    res.json(`todo with id ${id} was deleted successfully`)
+  } catch (err) {
+    console.error(err)
+  }
+})
 
 app.listen(5000, () => {
   console.log('server has started on port 5000')
